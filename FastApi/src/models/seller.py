72dd5fe18b9d2e0e478491bcf902f6424bea6cd9
putilin21dn <putilin21dn@yandex.ru>
2024-03-04 17:17:41 +0300

    Add fastapi

---
+++ FastApi/src/models/seller.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import BaseModel
+
+
+class Seller(BaseModel):
+    __tablename__ = "seller_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=False)
+    password: Mapped[str] = mapped_column(String(50), nullable=False)
+    books = relationship("Book", cascade="all,delete", backref="seller")
---
+++ FastApi/src/routers/v1/sellers.py
@@ -0,0 +1,93 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Response, status
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.configurations.database import get_async_session
+from src.models.books import Book
+from src.models.seller import Seller
+from src.schemas import IncomingSeller, ReturnedAllSellers, ReturnedSeller, ReturnedSellerWithBooks
+
+
+seller_router = APIRouter(tags=["seller"], prefix="/seller")
+
+DBSession = Annotated[AsyncSession, Depends(get_async_session)]
+
+
+@seller_router.post(
+    "/", response_model=ReturnedSeller, status_code=status.HTTP_201_CREATED
+)  # Прописываем модель ответа
+async def create_seller(seller: IncomingSeller, session: DBSession):
+    print(seller.__dict__)
+    new_seller = Seller(
+        first_name=seller.first_name,
+        last_name=seller.last_name,
+        email=seller.email,
+        password=seller.password,
+    )
+    session.add(new_seller)
+    await session.flush()
+
+    return new_seller
+
+
+# Ручка, возвращающая всех продавцов
+@seller_router.get("/", response_model=ReturnedAllSellers)
+async def get_all_sellers(session: DBSession):
+    query = select(Seller)
+    res = await session.execute(query)
+    sellers = res.scalars().all()
+    return {"sellers": sellers}
+
+
+# Ручка для получения продавца по его ИД
+@seller_router.get("/{seller_id}", response_model=ReturnedSellerWithBooks)
+async def get_seller(seller_id: int, session: DBSession):
+    if single_seller := await session.get(Seller, seller_id):
+        query = select(Book).filter(Book.seller_id == seller_id)
+        res = await session.execute(query)
+        books = res.scalars().all()
+
+        seller_data = {
+            "id": single_seller.id,
+            "first_name": single_seller.first_name,
+            "last_name": single_seller.last_name,
+            "email": single_seller.email,
+            "books": books,
+        }
+
+        return seller_data
+
+    return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+
+# Ручка для удаления продавца
+@seller_router.delete("/{seller_id}")
+async def delete_seller(seller_id: int, session: DBSession):
+    deleted_seller = await session.get(Seller, seller_id)
+    if deleted_seller:
+        await session.delete(deleted_seller)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+# Ручка для обновления данных о продавце
+@seller_router.put("/{seller_id}", response_model=ReturnedSeller)
+async def update_seller(
+    seller_id: int,
+    new_data: ReturnedSeller,
+    session: DBSession,
+):
+    
+    if updated_seller := await session.get(Seller, seller_id):
+        updated_seller.first_name = new_data.first_name
+        updated_seller.last_name = new_data.last_name
+        updated_seller.email = new_data.email
+
+        await session.flush()
+
+        return updated_seller
+
+    return Response(status_code=status.HTTP_404_NOT_FOUND)
